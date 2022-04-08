6dfaed8ad8371708a356c24ef9f12c00fc71a7be
Ivan <Mishkin_Ivan@mail.ru>
2022-04-08 10:13:25 +0400

    08-04-2022

--- Nortwind.Sample.net6/obj/Debug/net6.0-windows/View/CategoriesView.g.i.cs
+++ Nortwind.Sample.net6/obj/Debug/net6.0-windows/View/CategoriesView.g.i.cs
@@ -9,7 +9,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-using Microsoft.Windows.Themes;
 using Northwind.NET.Sample;
 using Northwind.NET.Sample.ViewModel;
 using System;
